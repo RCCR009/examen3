7493d1fb13ce4f346908bdf0df450fec8be730ff
Rodrigo París <rparisp@ucenfotec.ac.cr>
2018-12-15 01:29:17 -0600

    Montar verbos base de verbos http

--- src/main/java/com/cenfotec/examen3/controllers/PackageController.java
+++ src/main/java/com/cenfotec/examen3/controllers/PackageController.java
@@ -1,9 +1,15 @@
 package com.cenfotec.examen3.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,4 +27,29 @@ public class PackageController {
 		repository.save(ppackage);
 		return new ResponseEntity<>(HttpStatus.CREATED);
 	}
+	
+	@PutMapping("/package")
+	public ResponseEntity<String> putPackage(@RequestBody Package ppackage) {
+		repository.save(ppackage);
+		return new ResponseEntity<>(HttpStatus.ACCEPTED);
+	}
+	
+	@DeleteMapping("/package")
+	public ResponseEntity<String> deletePackage(@RequestBody Package ppackage) {
+		repository.delete(ppackage);
+		return new ResponseEntity<>(HttpStatus.MOVED_PERMANENTLY);
+	}
+	
+	@GetMapping("/package")
+	public List<Package> getPackage() {
+		List<Package> packages = repository.findAll();
+		return packages;
+	}
+	
+	@GetMapping("/package/{id}")
+	public Package getPackageById(@PathVariable("id") String id) {
+		Package pack = repository.findById(id).get();
+		return pack;
+	}
+	
 }
--- src/main/java/com/cenfotec/examen3/repository/FlightRepository.java
+++ src/main/java/com/cenfotec/examen3/repository/FlightRepository.java
@@ -1,13 +1,9 @@
 package com.cenfotec.examen3.repository;
 
-import java.util.List;
-
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import com.cenfotec.examen3.models.Flight;
 
 public interface FlightRepository extends MongoRepository<Flight, String>{
-
-	public List< Flight > findByName(String name);
 }
 
